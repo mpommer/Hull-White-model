57b23d8e7fcd0111dcb801bf00aea8f9ab5acc9d
mpommer <marcelpommer@web.de>
2021-10-24 13:41:51 +0200

    Application exampl
    Added a nice implementation of the use of the code which can be shown during the presentation.

--- Application_TrinomialTree.py
+++ Application_TrinomialTree.py
@@ -2,12 +2,12 @@
 """
 Created on Mon Oct 11 17:08:56 2021
 
-@author: marce
+@author: marcel Pommer
 """
 
 import numpy as np
 from TrinomialTree import treeConstruction, calculateBondPrice
-
+from tabulate import tabulate
 
 Zerocurve = np.array([[1, 0.03],
                       [2, 0.04],
@@ -17,26 +17,34 @@ Zerocurve = np.array([[1, 0.03],
                       [6, 0.08]])
 
 
-tree = treeConstruction(Zerocurve, lastDate=5, volatility=0.001, StepsPerYear = 72, a = 0)
+tree = treeConstruction(Zerocurve, lastDate=5, volatility=0.001, StepsPerYear = 36, a = 0)
 
-cashflow = np.array([[2.0, 1.0]])
-                     # [2., 0.0],
-                     # [3., 1.0]])
+paymentTimes = []
+cashFlows = []
+analyticPrices = []
+for index in range(5):
+    paymentTimes.append(index+1)
+    cashFlows.append(np.array([[index+1, 1.0]]))
+    
+    analyticPrices.append(np.exp(Zerocurve[::,1][index]*(-(index+1))))
+
                      
-# cashflow = np.array([[1.0, 0.1],
-#                       [2., 0.1],
-#                       [3., 1.0]])
 
-ExDates = np.array([[0., 0]])
-
-BondPrice = calculateBondPrice(tree, cashflow, ExDates)
-
-print(BondPrice)
+ExDates = np.array([[0., 0.0]])
 
 
-print("Ana :", 1/(1 +0.035)**2)
-print("Ana :", np.exp(-0.08))
+numericPrices = []
+relativeError = []
+for index, cashFlow in enumerate(cashFlows):
+    numericPrice = calculateBondPrice(tree, cashFlow, ExDates)
+    numericPrices.append(numericPrice)
+    
+    relativeError.append((numericPrice-analyticPrices[index])/analyticPrices[index] * 100)
 
-test = tree.TrinomialTreeparameters
 
-eld = test['Yield']
+data = np.array([paymentTimes, analyticPrices, numericPrices, relativeError]).T
+
+colNames = ['Payment Time', 'Analytic Price', 'Numeric Price', 'Relative Error']
+
+print(tabulate(data, headers = colNames))
+
